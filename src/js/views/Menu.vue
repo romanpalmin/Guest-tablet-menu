d609b40af068a615ef3b03fd181f266256d017a1
r.palmin <roman.palmin@gmail.com>
2017-01-19 17:56:17 +0300

    (task) Добавлено меню

---
+++ src/js/views/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>this is template body</div>
+        <div>{{msg}}</div>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#dddddd;
+    }
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue 11111'
+            }
+        },
+        components:{
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Menu from 'js/views/Menu.vue';
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+
+const routes = [
+    {path: '/menu', component: Menu},
+    {path: '/menu', component: Menu},
+    {path: '/menu', component: Menu},
+    {path: '/menu', component: Menu}
+];
+
+var router = new VueRouter({
+    linkActiveClass: 'active-class',
+    routes
+});
+
+
+/* Bootstrap routes to the main component */
+const app = new Vue({
+    router,
+    template: `
+    <div id="app">
+        <nav class="pages-nav">
+            <div class="pages-nav__item "><router-link to="/menu"><a class="link-page link">Сайт</a></router-link></div>
+            <div class="pages-nav__item "><router-link to="/menu"><a class="link-page link">Развлечения</a></router-link></div>
+            <div class="pages-nav__item "><router-link to="/menu"><a class="link-page link">Меню</a></router-link></div>
+            <div class="pages-nav__item "><router-link to="/menu"><a class="link-page link">Вы выбрали</a></router-link></div>
+        </nav>
+
+      <router-view class="view"></router-view>
+    </div>`
+}).$mount('#app');
