b33d305d8cec9bd1bb60d2e87a01be5eb0e12585
r.palmin <roman.palmin@gmail.com>
2017-02-08 15:16:35 +0300

    Добалвены настройки и файл из которого будут браться настрйоки в перспективе

---
+++ src/js/views/components/store/currentStates.js
@@ -0,0 +1,11 @@
+export default {
+    MenuPoints:[],
+    settings:{
+        server:'http://10.10.250.61/',
+        urlSmallImage:'img/',
+        urlBigImage:'images/',
+        images:{
+            logo: 'beer-zha.png'
+        }
+    }
+}
