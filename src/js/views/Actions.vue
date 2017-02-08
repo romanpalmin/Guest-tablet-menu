66a7d751a8d8545059a4ee51e4081d494f98bc5d
r.palmin <roman.palmin@gmail.com>
2017-02-08 15:17:11 +0300

    Добавлены страницы "Фишки" и "Акции"

---
+++ src/js/views/Actions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="content">
+        <div content="inner-content">Здесь будут описания акций {{msg}}</div>
+    </div>
+</template>
+<style scoped lang="less">
+    .content {
+        width: 100%;
+        padding-top: 100px;
+        text-align: center;
+        .inner-content {
+        }
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'Стрит фуд бар'
+            }
+        }
+    }
+
+
+</script>
---
+++ src/js/views/Fishki.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="content">
+        <div class="inner-content">Здесь будут какие-то фишки ресторана {{msg}}</div>
+    </div>
+</template>
+<style scoped lang="less">
+    .content {
+        width: 100%;
+        padding-top: 100px;
+        text-align: center;
+        .inner-content {
+
+        }
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'Фуд стрит бар'
+            }
+        }
+    }
+
+
+</script>
