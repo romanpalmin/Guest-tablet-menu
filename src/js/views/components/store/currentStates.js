12c51233c4c3d4c053cca2f71d9f8304e0a6acec
r.palmin <roman.palmin@gmail.com>
2017-02-09 17:27:37 +0300

    Добавлено состояние для загруженных позиций и удалено ненужное хранилище

--- src/js/views/components/store/currentStates.js
+++ src/js/views/components/store/currentStates.js
@@ -1,12 +1,55 @@
 export default {
-    MenuPoints:[],
     settings:{
         server:'http://10.10.250.61/',
+       /* server:'http://10.10.182.11/',*/
         urlSmallImage:'img/',
         urlBigImage:'images/',
+        urlBackImage: 'images',
         images:{
             logo: 'beer-zha.png',
             close: 'close.png'
+        },
+        testMode:false
+    },
+    appState:{
+        MenuPoints:[],
+        Category:{
+            "332020":{
+                name: 'Бургеры',
+                currentState:[]
+            },
+            "342020":{
+                name: 'СОСИДЖ ФЭКТОРИ',
+                currentState:[]
+            },
+            "352020":{
+                name: 'BBQ',
+                currentState:[]
+            },
+            "392020":{
+                name: 'БЭЙКС ЭНД СНЭКС',
+                currentState:[]
+            },
+            "412020":{
+                name: '4STARTS',
+                currentState:[]
+            },
+            "422020":{
+                name: 'АЙСИ-СПАЙСИ',
+                currentState:[]
+            },
+            "432020":{
+                name: 'НАПИТКИ',
+                currentState:[]
+            },
+            "462020":{
+                name: 'ПИВО',
+                currentState:[]
+            }
+        },
+        show: {
+            name: 'Развлечения',
+            currentState:[]
         }
     }
 }
