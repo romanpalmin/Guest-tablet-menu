bd3d72bc428a7446113321765a838eb1a4d2aacb
r.palmin <roman.palmin@gmail.com>
2017-02-10 14:10:02 +0300

    Сверстана страница Корзины

---
+++ src/js/Order.vue
@@ -0,0 +1,202 @@
+<template>
+    <div>
+        <div class="container">
+
+            <div class="logo">
+                <img :src=this.urlLogo class="logo-style">
+            </div>
+
+            <div class="content" id="content">
+
+                <table class="main-table">
+                    <tr class="table-row ">
+                        <td colspan="3" class="table-header">Вы выбрали:</td>
+                    </tr>
+                    <tr class="table-row">
+                        <td class="table-cell col-header col1">КОЛ-ВО</td>
+                        <td class="table-cell col-header col2">НАИМЕНОВАНИЕ</td>
+                        <td class="table-cell col-header col3"></td>
+                    </tr>
+                    <template>
+                        <tr class="table-row">
+                            <td class="table-cell col1">1</td>
+                            <td class="table-cell col2">Хрустящие креветки с соусом Терияки</td>
+                            <td class="table-cell col3"><img class="delete" data-code="itemid" @click="deleteOrder"
+                                                             :src="urlClose"></td>
+                        </tr>
+                        <tr class="table-row">
+                            <td class="table-cell col1">1</td>
+                            <td class="table-cell col2">Хрустящие креветки с соусом Терияки</td>
+                            <td class="table-cell col3"><img class="delete" data-code="itemid" @click="deleteOrder"
+                                                             :src="urlClose"></td>
+                        </tr>
+                    </template>
+                    <template class="table-footer">
+                        <tr>
+                            <td class="table-header"></td>
+                            <td class="table-header"></td>
+                            <td class="table-header"></td>
+                        </tr>
+                    </template>
+                    <tr class="table-row ">
+                        <td colspan="3" class="footer">
+                            <div class="btn-wrapper">
+                                <div class="btn delete-all">Удалить все выбранное</div>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="less">
+    .container {
+        .logo-style {
+            width: 240px;
+            padding-left: 30px;
+            padding-right: 30px;
+            padding-top: 60px;
+        }
+
+        .logo {
+            display: block;
+            width: 300px;
+            height: 100vh;
+            background-color: #F7F7F7;
+            top: 60px;
+            left: 0;
+            position: absolute;
+        }
+        .content {
+            padding-top: 60px;
+            padding-left: 301px;
+
+            .main-table {
+                border-collapse: collapse;
+                width: 90%;
+                margin: 30px;
+                .delete {
+                    cursor: pointer;
+                    width: 30px;
+                    height: 30px;
+                    img {
+                        width: 100%;
+                    }
+                }
+                .table-header {
+
+                }
+
+                .table-header {
+                    color: #FFFFFF;
+                    font-family: sans-serif;
+                    font-size: 60px;
+                    width: calc(100vw - 311px);
+                    height: 99px;
+                    top: 0;
+                }
+                .table-row {
+                    text-align: left;
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+
+                    height: 24px;
+                    .footer {
+                        .btn-wrapper {
+                            width: 100%;
+                            .btn {
+                                text-align: center;
+                                line-height: 39pt;
+                                width: 350px;
+                                margin: 0 auto;
+                                color: #000000;
+                                background-color: #F7F7F7;
+                                font-family: sans-serif;
+                                font-size: 28px;
+                                border-radius: 15px;
+                                user-select: none;
+
+                            }
+                        }
+                        border-bottom: none;
+
+                    }
+
+                }
+                .table-cell {
+                    border-bottom: #8A8A8A 1px solid;
+                    color: #F7F7F7;
+                    font-family: sans-serif;
+                    font-size: 18px;
+                    &.col1 {
+                        text-align: center;
+                    }
+                    padding-left: 20px;
+                    height: 50px;
+                    border-bottom: #8A8A8A 1px solid;
+                    &.col-header {
+                        text-align: center;
+                        border-bottom: #8A8A8A 4px solid;
+                        border-top: #8A8A8A 4px solid;
+                        font-family: sans-serif;
+                        font-size: 18px;
+                        &.col1 {
+                            width: 15%;
+                        }
+                        &.col2 {
+                            width: 75%;
+                        }
+                        &.col3 {
+                            width: 10%;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+</style>
+<script>
+    import orders from './components/data/orders';
+    import sets from './components/store/currentStates';
+    export default{
+        data(){
+            return {
+                positions: orders,
+                urlLogo: '',
+                urlClose: '',
+                itemId: []
+            }
+        },
+
+        methods:{
+            deleteOrder: function(evt){
+                console.log(+evt.target.dataset.code);
+            }
+        },
+        mounted(){
+            console.log(sets.settings);
+            this.urlLogo = sets.settings.server + sets.settings.urlSmallImage + sets.settings.images.logo;
+            this.urlClose = sets.settings.server + sets.settings.urlSmallImage + sets.settings.images.close;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
