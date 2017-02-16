162328b51422e08f99456dba130c86b33f472079
r.palmin <roman.palmin@gmail.com>
2017-02-16 14:16:36 +0300

    Удалены лишние логи и ненужные файлы

--- src/js/components/helpers/ajax.js
+++ src/js/components/helpers/ajax.js
@@ -6,7 +6,7 @@ const server = `${ip}/`;
 const user = 'tab01';
 const password = '01';
 const baseUrl = '/menu/hs/model?';
-var ajaxServerUrlShort = `http://${user}:${password}@${server}${baseUrl}`;
+let ajaxServerUrlShort = `http://${user}:${password}@${server}${baseUrl}`;
 
 function executeRequest(url, callback) {
     axios.get(ajaxServerUrlShort + url)
@@ -24,31 +24,24 @@ function getUrl(operation){
     let url = '';
     switch (operation.name){
         case 'categories':
-            console.log(operation);
             url = 'groups=';
             break;
         case 'positions':
-            console.log(operation);
             url = `groups=${operation.catId}&category=${operation.catId}`;
             break;
         case 'show':
-            console.log(operation);
             url = 'groups=1&shows=1';
             break;
         case 'order':
-            console.log(operation);
             url = 'groups=1&korzina=1';
             break;
         case 'clearOrder':
-            console.log(operation);
             url = 'groups=342020&delcart=1&tovar=1';
             break;
         case 'deleteFromOrder':
-            console.log(operation);
             url = `groups=342020&tovar=${operation.id}&dellcartitem=${operation.stroka}`;
             break;
         case 'addToOrder':
-            console.log(operation);
             url = `groups=1&addcart=1&tovar=${operation.positionId}`;
             break;
         case 'showLamp':
