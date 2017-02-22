e6365f5e6cbd519fa2f2c0597beb30189bcefcc8
r.palmin <roman.palmin@gmail.com>
2017-02-22 15:54:15 +0300

    Реализована проверка на признак ActionTime

--- src/js/components/MainMenu.vue
+++ src/js/components/MainMenu.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div class="main-menu" v-if="count_of_ctgs === 8">
+        <div class="main-menu" v-if="!count_of_main">
             <ul class="root-icons">
-                <li class="root-icon" v-for="item in ctgs_with_params">
+                <li class="root-icon" v-for="item in tabView">
                     <router-link :to="item.route">
                         <a :root-data-code="item.code">
                             <div class="root-icon-image" :style="item.style">
@@ -18,24 +18,25 @@
         <div v-else class="main-table">
             <table class="inner-table">
                 <tr class="top-row">
-                    <td colspan="8">
-                        <div class="img-wrapper" v-for="item in main">
-                            <router-link :to="item.route">
-                                <a >
-                                    <div class="root-icon-image-bottom">
-                                        <img :src="getImageSrcBig(item)">
-                                        <div class="root-icon-descr">
-                                            {{ item.name }}
+                    <td :colspan="ctg_bottom_count">
+                        <template>
+                            <div class="img-wrapper" v-for="item in mainView">
+                                <router-link :to="item.route">
+                                    <a>
+                                        <div class="root-icon-image-bottom">
+                                            <img :src="getImageSrcBig(item)">
+                                            <div class="root-icon-descr">
+                                                {{ item.name }}
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
-                            </router-link>
-                        </div>
+                                    </a>
+                                </router-link>
+                            </div>
+                        </template>
                     </td>
                 </tr>
-
                 <tr>
-                    <template v-for="item in ctgs_with_params_without_adding">
+                    <template v-for="item in ctgs_bottom">
                         <td class="current-cell">
                             <router-link :to="item.route">
                                 <a :root-data-code="item.code">
@@ -53,15 +54,17 @@
             </table>
         </div>
     </div>
+
 </template>
+
 <style scoped lang="less">
     .main-table {
-        .img-wrapper{
-            max-width: 300px;
-            max-height: 300px;
+        .img-wrapper {
+            max-width: 500px;
+            max-height: 500px;
             margin: 0 auto;
-            img{
-                width:100%;
+            img {
+                width: 100%;
             }
         }
         .root-icon-descr {
@@ -78,7 +81,7 @@
             height: 100%;
         }
         .top-row {
-            height: 600px;
+            height: 800px;
             vertical-align: middle;
         }
 
@@ -87,6 +90,8 @@
     }
 
     .main-menu {
+        margin-left: 35px;
+
         .root-icon-descr {
             color: #FFFFFF;
             position: relative;
@@ -117,8 +122,9 @@
             display: inline-block;
             width: 280px;
             height: 315px;
-            margin-top: 30px;
-            margin-right: 30px;
+            margin-top: 35px;
+            margin-right: 35px;
+            margin-left: 35px;
         }
 
         a {
@@ -130,90 +136,107 @@
 
 </style>
 <script>
-    import state from './store/currentStates';
-    import ajax from './helpers/ajax.js';
-    import _ from 'lodash';
-    export default{
-        data(){
-            return {
-                ctgs: [],
-                main: [],
-                count: 8
-            }
-        },
-        computed: {
-            count_of_ctgs: function(){
-                return this.ctgs.length;
-            },
-
-            ctgs_with_params: function () {
-                var res = this.ctgs.map(function (item) {
-                    item.style = 'background-image: url(' +  state.settings.server + state.settings.urlBigImage + item.urlSmallImage + ');';
-                    item.route = 'menu/'+item.code;
-                    return item;
-                });
-                return res;
-            },
-            ctgs_with_params_without_adding: function () {
-                var res = this.ctgs.map(function (item) {
-                    item.route = 'menu/'+item.code;
-                    return item;
-                });
-                res = _.without(res, _.find(res, {code: '472020'}));
-                res = _.without(res, _.find(res, {code: '482020'}));
-                return res;
-            },
-            main_position: function(){
-                return this.main[0]
-            },
-
-            isTablet: function(){
-                return state.settings.isTablet;
-            }
-        },
-
-        methods:{
-            getImageSrc(item){
-                return state.settings.server + state.settings.urlBigImage + item.urlSmallImage;
-            },
-            getImageSrcBig(item){
-                return state.settings.server + state.settings.urlBigImage + item.urlBigImage;
-            },
-            getMainPosition(){
-                var res = [];
-                var res = this.ctgs.map(function (item) {
-                    if (item.code === '472020' || item.code === '482020'){
-                        item.route = 'menu/'+item.code;
-                        return item;
-                    }
-                });
-                var ret = _.remove(res, function(item){
-                    return item !== undefined;
-                });
-                return ret;
-                }
-            },
-        mounted(){
-             var self = this;
-             const operation = {};
-             if (state.appState.MenuPoints.length > 0){
-                 self.ctgs = state.appState.MenuPoints;
-                 self.main = self.getMainPosition();
-                 } else {
-                    operation.name = 'categories';
-                    ajax.exec(operation, function(resp){
-                        self.ctgs = resp.data;
-                        state.appState.MenuPoints = resp.data;
-                        self.main = self.getMainPosition();
-                 });
-             }
-             this.isTablet = state.settings.isTablet;
-             if (state.settings.isTablet) {
-                console.log('Планшет');
-             }
-             else {
-                console.log('Уличный стенд');
-             }
+import state from './store/currentStates';
+import ajax from './helpers/ajax.js';
+import _ from 'lodash';
+export default {
+    data(){
+        return {
+            ctgs: state.appState.MenuPoints
         }
+    },
+
+    watch: {
+        ctgs: function (val) {
+        }
+
+    },
+    computed: {
+        count_of_main: function () {
+            let res = _.filter(this.ctgs, {activeTime: '1'});
+            return res.length > 0;
+        },
+
+        tabView: function () {
+            let res = this.ctgs.map(function (item) {
+                item.route = 'menu/' + item.code;
+                item.style = 'background-image: url(' + state.settings.server + state.settings.urlBigImage + item.urlSmallImage + ');';
+                return item;
+            });
+            return res;
+        },
+
+        mainView: function () {
+            let result = [];
+            var res = [];
+            var res = this.ctgs.map(function (item) {
+                if (item.activeTime && item.activeTime === '1') {
+                    item.route = 'menu/' + item.code;
+                    return item;
+                }
+            });
+            var ret = _.remove(res, function (item) {
+                return item !== undefined;
+            });
+            result.push(ret[0])
+            return result;
+        },
+
+        ctgs_bottom: function () {
+            var res = this.ctgs.map(function (item) {
+                item.route = 'menu/' + item.code;
+                return item;
+            });
+
+            res = _.without(res, _.find(res, {activeTime: '1'}));
+            return res;
+        },
+
+        ctg_bottom_count: function () {
+            let res = _.without(this.ctgs, _.find(this.ctgs, {activeTime: '1'}));
+            return res.length;
+        }
+    },
+
+    methods: {
+        getImageSrc(item){
+            return state.settings.server + state.settings.urlBigImage + item.urlSmallImage;
+        },
+        getImageSrcBig(item){
+            return state.settings.server + state.settings.urlBigImage + item.urlBigImage;
+        },
+        getResponce(){
+            var self = this;
+            const operation = {};
+            operation.name = 'categories';
+            ajax.exec(operation, function (resp) {
+                self.ctgs = resp.data;
+                _.find(self.ctgs, {code: '482020'}).activeTime = '1';
+                state.appState.MenuPoints = self.ctgs;
+            });
+        }
+    },
+
+    mounted(){
+        var self = this;
+
+        if (state.appState.MenuPoints.length > 0) {
+            self.ctgs = state.appState.MenuPoints;
+        } else {
+            this.getResponce();
+        }
+
+        if (state.settings.isTablet) {
+            console.log('Планшет');
+        }
+        else {
+            console.log('Уличный стенд');
+        }
+
+        let upTimer = setInterval(function () {
+            self.getResponce();
+        }, 15000);
     }
+}
+
 </script>
