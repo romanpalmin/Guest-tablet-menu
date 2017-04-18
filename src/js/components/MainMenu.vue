a35935749b79e852f043b7136e7458d944754e64
r.palmin <roman.palmin@gmail.com>
2017-04-18 14:08:34 +0300

    * исправлено форматирование * добавлен 11 элемент * раположен на street-menu2/new

--- src/js/components/MainMenu.vue
+++ src/js/components/MainMenu.vue
@@ -8,6 +8,7 @@
                             <div class="root-icon-image" :style="item.style">
                                 <div class="root-icon-descr">
                                     {{ item.name }}
+
                                 </div>
                             </div>
                         </a>
@@ -18,7 +19,7 @@
         <div v-else class="main-table">
             <table class="inner-table">
                 <tr class="top-row">
-                    <td colspan=9>
+                    <td colspan=10>
                         <template>
                             <div class="img-wrapper">
                                 <router-link :to="mainPosition.route">
@@ -27,6 +28,7 @@
                                             <div id="svg" class="svg"></div>
                                             <div class="root-icon-descr">
                                                 {{ mainPosition.name }}
+
                                             </div>
                                         </div>
                                     </a>
@@ -46,6 +48,7 @@
                                         </div>
                                         <div class="root-icon-descr">
                                             {{ item.name }}
+
                                         </div>
                                     </div>
                                 </a>
@@ -62,11 +65,11 @@
 <style scoped lang="less">
     .current-cell {
         .img-wrapper {
-            width: 200px;
+            width: 170px;
             img {
                 width: 100%;
             }
-            .svg{
+            .svg {
                 width: 500px;
             }
         }
@@ -74,6 +77,7 @@
 
     .main-menu {
         margin-left: 35px;
+        text-align: center;
 
         .root-icon-descr {
             color: #FFFFFF;
@@ -90,8 +94,8 @@
 
         .root-icon-image {
             background-size: cover;
-            width: 280px;
-            height: 280px;
+            width: 210px;
+            height: 210px;
         }
 
         .root-icons {
@@ -103,9 +107,9 @@
 
         .root-icon {
             display: inline-block;
-            width: 280px;
-            height: 315px;
-            margin-top: 35px;
+            width: 200px;
+            height: 290px;
+            margin-top: 75px;
             margin-right: 35px;
             margin-left: 35px;
         }
@@ -148,173 +152,176 @@
 </style>
 <script>
 
-import state from './store/currentStates';
-import ajax from './helpers/ajax.js';
-import axios from 'axios';
-import _ from 'lodash';
-//require ('../vendor/snap.svg-min.js');
-//require ('../vendor/SnapSVGAnimator.min.js');
-export default {
-    data(){
-        return {
-            ctgs: [],
-            showTabletView: true,
-            mainPosition: {},
-            isDebug:false,
-            svg: {
-                jsonfile : '',
-                fps : 24,
-                width : 200,
-                height : 200,
-                AJAX_req : ''
-            },
-            svgUrl: '',
-            showSvg: true
-        }
-    },
-
-    watch:{
-        ctgs: function(){
-            this.showTabletView = this.defineView();
-            if (!this.showTabletView){
-                this.mainPosition = this.populateMainPosition();
+    import state from './store/currentStates';
+    import ajax from './helpers/ajax.js';
+    import axios from 'axios';
+    import _ from 'lodash';
+    //require ('../vendor/snap.svg-min.js');
+    //require ('../vendor/SnapSVGAnimator.min.js');
+    export default {
+        data(){
+            return {
+                ctgs: [],
+                showTabletView: true,
+                mainPosition: {},
+                isDebug: true,
+                svg: {
+                    jsonfile: '',
+                    fps: 24,
+                    width: 200,
+                    height: 200,
+                    AJAX_req: ''
+                },
+                svgUrl: '',
+                showSvg: true
             }
-        }
-    },
-
-    computed: {
-        tabView: function () {
-            let res = this.ctgs.map(function (item) {
-                item.route = 'menu/' + item.code;
-                item.style = 'background-image: url(' + state.settings.server + state.settings.urlBigImage + item.urlSmallImage + ');';
-                return item;
-            });
-            return res;
         },
 
-        ctgs_bottom: function () {
-            let mainDish = this.populateMainPosition();
-            let res = this.ctgs.map(function (item) {
-                item.route = 'menu/' + item.code;
-                return item;
-            });
-            res = _.filter(res, function(item){return item.code !== mainDish.code});
-            return res;
+        watch: {
+            ctgs: function () {
+                this.showTabletView = this.defineView();
+                if (!this.showTabletView) {
+                    this.mainPosition = this.populateMainPosition();
+                }
+            }
         },
-    },
 
-    methods: {
-        getResponce(){
-            var self = this;
-            const operation = {};
-            operation.name = 'categories';
-            ajax.exec(operation, function (resp) {
-                self.ctgs = resp.data;
-                state.appState.MenuPoints = self.ctgs;
-            });
-        },
-        getJsonCtgs(){
-            var self = this;
-            const operation = {};
-            this.axios.get('./ctgs.js')
-                .then(function (response) {
-                   self.ctgs = response.data;
-                   state.appState.MenuPoints = self.ctgs;
-                })
-                .catch(function (error) {
-                    console.log(error);
+        computed: {
+            tabView: function () {
+                let res = this.ctgs.map(function (item) {
+                    item.route = 'menu/' + item.code;
+                    item.style = 'background-image: url(' + state.settings.server + state.settings.urlBigImage + item.urlSmallImage + ');';
+                    return item;
                 });
+                return res;
+            },
+
+            ctgs_bottom: function () {
+                let mainDish = this.populateMainPosition();
+                let res = this.ctgs.map(function (item) {
+                    item.route = 'menu/' + item.code;
+                    return item;
+                });
+                res = _.filter(res, function (item) {
+                    return item.code !== mainDish.code
+                });
+                return res;
+            },
         },
 
-        getData(){
-            if (this.isDebug){
-                this.getJsonCtgs();
-            }
-            else {
-                this.getResponce();
-            }
-        },
+        methods: {
+            getResponce(){
+                var self = this;
+                const operation = {};
+                operation.name = 'categories';
+                ajax.exec(operation, function (resp) {
+                    self.ctgs = resp.data;
+                    state.appState.MenuPoints = self.ctgs;
+                });
+            },
+            getJsonCtgs(){
+                var self = this;
+                const operation = {};
+                this.axios.get('./ctgs.js')
+                    .then(function (response) {
+                        self.ctgs = response.data;
+                        state.appState.MenuPoints = self.ctgs;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
 
-        defineView(){
-            let notActive = _.filter(this.ctgs, {activeTime: '0'});
-            return (notActive.length === 2);
-        },
-
-        populateMainPosition(){
-            let ret = {};
-            ret = _.filter(this.ctgs, function(o){return o.activeTime === '1' && (o.breakfast === '1' || o.lanhc === '1')});
-             if (ret.length > 0) {
-                ret[0].route = 'menu/' + ret[0].code;
-                ret[0].img = state.settings.server + state.settings.urlBigImage + ret[0].urlBigImage;
-                this.svgUrl = 'assets/svg/' + ret[0].code + '.json';
-                if (this.svgUrl !== this.svg.json) {
-                    this.showSvg = true;
+            getData(){
+                if (this.isDebug) {
+                    this.getJsonCtgs();
                 }
-                this.svg.json = this.svgUrl;
-                this.AJAX_JSON_Req(this.svg.json);
-                return ret[0];
-             }
-        },
-        getImageSrc(item){
-            return state.settings.server + state.settings.urlBigImage + item.urlSmallImage;
-        },
+                else {
+                    this.getResponce();
+                }
+            },
 
-        getImageSrcBig(item){
-            return state.settings.server + state.settings.urlBigImage + item.urlBigImage;
-        },
+            defineView(){
+                let notActive = _.filter(this.ctgs, {activeTime: '0'});
+                return (notActive.length === 2);
+            },
 
-        handle_AJAX_Complete() {
-            if( this.svg.AJAX_req.readyState === 4 && this.svg.AJAX_req.status === 200 )
+            populateMainPosition(){
+                let ret = {};
+                ret = _.filter(this.ctgs, function (o) {
+                    return o.activeTime === '1' && (o.breakfast === '1' || o.lanhc === '1')
+                });
+
+                if (ret.length > 0) {
+                    ret[0].route = 'menu/' + ret[0].code;
+                    ret[0].img = state.settings.server + state.settings.urlBigImage + ret[0].urlBigImage;
+                    this.svgUrl = 'assets/svg/' + ret[0].code + '.json';
+                    if (this.svgUrl !== this.svg.json) {
+                        this.showSvg = true;
+                    }
+                    this.svg.json = this.svgUrl;
+                    this.AJAX_JSON_Req(this.svg.json);
+                    return ret[0];
+                }
+            },
+            getImageSrc(item){
+                return state.settings.server + state.settings.urlBigImage + item.urlSmallImage;
+            },
+
+            getImageSrcBig(item){
+                return state.settings.server + state.settings.urlBigImage + item.urlBigImage;
+            },
+
+            handle_AJAX_Complete() {
+                if (this.svg.AJAX_req.readyState === 4 && this.svg.AJAX_req.status === 200) {
+                    if (this.showSvg) {
+                        const json = JSON.parse(this.svg.AJAX_req.responseText);
+                        var container = document.getElementById("svg");
+                        container.innerHTML = '';
+
+                        const comp = new SVGAnim(
+                            json,
+                            this.svg.width,
+                            this.svg.height,
+                            this.svg.fps
+                        );
+
+
+                        container.appendChild(comp.s.node);
+                        this.showSvg = false;
+                    } else {
+                    }
+                }
+            },
+
+            AJAX_JSON_Req(url)
             {
-                if (this.showSvg){
-                    const json = JSON.parse(this.svg.AJAX_req.responseText);
-                    var container = document.getElementById("svg");
-                    container.innerHTML = '';
+                this.svg.AJAX_req = new XMLHttpRequest();
+                this.svg.AJAX_req.open("GET", url, true);
+                this.svg.AJAX_req.setRequestHeader("Content-type", "application/json");
 
-                    const comp = new SVGAnim(
-                               json,
-                               this.svg.width,
-                               this.svg.height,
-                               this.svg.fps
-                               );
-
-
-                    container.appendChild(comp.s.node);
-                    this.showSvg = false;
-                } else {
-                }
+                this.svg.AJAX_req.onreadystatechange = this.handle_AJAX_Complete;
+                this.svg.AJAX_req.send();
             }
+
         },
 
-        AJAX_JSON_Req( url )
-        {
-            this.svg.AJAX_req = new XMLHttpRequest();
-            this.svg.AJAX_req.open("GET", url, true);
-            this.svg.AJAX_req.setRequestHeader("Content-type", "application/json");
+        mounted(){
+            const self = this;
+            if (state.appState.MenuPoints.length > 0) {
+                self.ctgs = state.appState.MenuPoints;
+            } else {
+                this.getData();
+            }
 
-            this.svg.AJAX_req.onreadystatechange = this.handle_AJAX_Complete;
-            this.svg.AJAX_req.send();
+            let title = state.settings.isTablet ? 'Планшет' : 'Уличный стенд';
+            console.log(title);
+
+            let upTimer = setInterval(function () {
+                self.getData();
+            }, 15000);
         }
-
-    },
-
-    mounted(){
-        const self = this;
-        if (state.appState.MenuPoints.length > 0) {
-            self.ctgs = state.appState.MenuPoints;
-        } else {
-           this.getData();
-        }
-
-
-        let title = state.settings.isTablet ? 'Планшет' : 'Уличный стенд';
-        //console.log(title);
-
-        let upTimer = setInterval(function () {
-             self.getData();
-        }, 15000);
     }
-}
 
 
 </script>
