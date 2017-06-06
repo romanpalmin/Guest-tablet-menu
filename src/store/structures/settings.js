81a07ba54f59ec6a6f4e8ccd42ae5cec8991c3be
r.palmin <roman.palmin@gmail.com>
2017-06-06 16:55:41 +0300

    *  server at .11

--- src/store/structures/settings.js
+++ src/store/structures/settings.js
@@ -5,8 +5,8 @@ export default {
     password: '',
     /* server:'http://10.10.182.11/',*/
     /*urlImagesBase:  'http://10.100.50.248/',*/
-    /*urlBase:  'http://10.10.182.11/',*/
-    urlBase:  'http://10.100.50.248/',
+    urlBase:  'http://10.10.182.11/',
+    /*urlBase:  'http://10.100.50.248/',*/
     urlSmallImage: 'img/',
     urlBigImage: 'images/',
     urlBackImage: 'images',
@@ -25,5 +25,5 @@ export default {
     language: 'ru',
     showLanguageToggle: false,
     localPath:'StreetFoodBar/images',
-    isBrowser: true
+    isBrowser: false
 }
