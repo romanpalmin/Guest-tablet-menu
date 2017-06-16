f66a8ced58ceb43dbeeda463bb69d1f9ffd6bfbe
r.palmin <roman.palmin@gmail.com>
2017-06-16 14:05:49 +0300

    *  Изменены наименования групп в справочнике

--- src/store/structures/categoryPositions.js
+++ src/store/structures/categoryPositions.js
@@ -1,43 +1,135 @@
 export default {
     "332020": {
+     name: 'Бургеры',
+     currentState: []
+     },
+     "342020": {
+     name: 'СОСИДЖ ФЭКТОРИ',
+     currentState: []
+     },
+     "352020": {
+     name: 'BBQ',
+     currentState: []
+     },
+     "392020": {
+     name: 'БЭЙКС ЭНД СНЭКС',
+     currentState: []
+     },
+     "412020": {
+     name: '4STARTS',
+     currentState: []
+     },
+     "422020": {
+     name: 'АЙСИ-СПАЙСИ',
+     currentState: []
+     },
+     "432020": {
+     name: 'НАПИТКИ',
+     currentState: []
+     },
+     "462020": {
+     name: 'ПИВО',
+     currentState: []
+     },
+     "472020": {
+     name: "ЗАВТРАКИ",
+     currentState: []
+     },
+     "482020": {
+     name: "БИЗНЕС ЛАНЧ",
+     currentState: []
+     },
+     // new category
+
+     "515150": {
+     name: 'Бургеры',
+     currentState: []
+     },
+     "515250": {
+     name: 'СОСИДЖ ФЭКТОРИ',
+     currentState: []
+     },
+     "515350": {
+     name: 'BBQ',
+     currentState: []
+     },
+     "515750": {
+     name: 'БЭЙКС ЭНД СНЭКС',
+     currentState: []
+     },
+     "524950": {
+     name: '4STARTS',
+     currentState: []
+     },
+     "525050": {
+     name: 'АЙСИ-СПАЙСИ',
+     currentState: []
+     },
+     "525150": {
+     name: 'НАПИТКИ',
+     currentState: []
+     },
+     "525450": {
+     name: 'ПИВО',
+     currentState: []
+     },
+     "525550": {
+     name: "ЗАВТРАКИ",
+     currentState: []
+     },
+     "525650": {
+     name: "БИЗНЕС ЛАНЧ",
+     currentState: []
+     },
+     "524350": {
+     name: "SUMMER TIME",
+     currentState: []
+     },
+
+    // new category 2
+
+    "513232": {
         name: 'Бургеры',
         currentState: []
     },
-    "342020": {
+    "523232": {
         name: 'СОСИДЖ ФЭКТОРИ',
         currentState: []
     },
-    "352020": {
+    "533232": {
         name: 'BBQ',
         currentState: []
     },
-    "392020": {
+    "573232": {
         name: 'БЭЙКС ЭНД СНЭКС',
         currentState: []
     },
-    "412020": {
+    "653232": {
         name: '4STARTS',
         currentState: []
     },
-    "422020": {
+    "663232": {
         name: 'АЙСИ-СПАЙСИ',
         currentState: []
     },
-    "432020": {
+    "673232": {
         name: 'НАПИТКИ',
         currentState: []
     },
-    "462020": {
+    "703232": {
         name: 'ПИВО',
         currentState: []
     },
-    "472020": {
+    "713232": {
         name: "ЗАВТРАКИ",
         currentState: []
     },
-    "482020": {
+    "723232": {
         name: "БИЗНЕС ЛАНЧ",
         currentState: []
+    },
+    "743232": {
+        name: "SUMMER TIME",
+        currentState: []
     }
-
 }
