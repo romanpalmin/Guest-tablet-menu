898da7153b7ab855daf2e0706dcf63ca4785a4f2
r.palmin <roman.palmin@gmail.com>
2017-08-01 13:55:39 +0300

    * Изменен URL на тестовый

--- src/js/components/helpers/ajax.js
+++ src/js/components/helpers/ajax.js
@@ -8,7 +8,7 @@ const ip = '10.100.50.248';
 let userName = 'planshet';
 let password = 'planshet';
 let ajaxServerUrlShort = '';
-const baseUrl = 'menu/hs/model?';
+const baseUrl = 'planshet/hs/model?';
 //-------------------------------
 ajaxServerUrlShort = formAjaxVars();
 //console.log('ip: ' + ip);
